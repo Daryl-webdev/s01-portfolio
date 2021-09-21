c861d04133096bf127ad28f6374031dbf9d2e873
Daryl-webdev <grimaldomarkdaryl@gmail.com>
2021-09-21 21:00:52 +0800

    Includes activity code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,13 @@ import Nav from 'react-bootstrap/Nav'
 
 export default function AppNavbar() {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="primary" className="navbar-dark" expand="lg">
         <Navbar.Brand href="#home">Daryl G.</Navbar.Brand>
-            <Nav className="mr-auto">
+            <Nav className="ml-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#about">About</Nav.Link>
                 <Nav.Link href="#link">Link</Nav.Link>
+                <Nav.Link href="#about">About</Nav.Link>
+                <Nav.Link href="#contacts">Contacts</Nav.Link>
             </Nav>
         </Navbar>
     )
